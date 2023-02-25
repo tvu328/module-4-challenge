0dda5ed1712c89f56c10c39d285c3878b8bc0069
Tyler <tylerlvvu@gmail.com>
2023-02-25 00:09:51 -0800

    third update

--- README.md
+++ README.md
@@ -4,5 +4,5 @@
 This code quiz was designed and mader to test a skills on the history of the NBA and to create a quiz with coding from scratch. It is a simple but effective quiz that does the job and can rotate questions and is responsive to users. I learned more HTML and how to gradually work on my progress. 
 
 ## Usage
-screenshot
+![ScreenShot](screenshot.png?raw=true "QuizHomePage")
 
--- script.js
+++ script.js
@@ -5,50 +5,50 @@ var scoresDiv = document.querySelector(".scores")
 var timerEl = document.getElementById("time")
 var timeleft = 60
 
-function tick (){
+function tick() {
     timeleft--
-    if (timeleft<=0){
-        timeleft=0
+    if (timeleft <= 0) {
+        timeleft = 0
         endgame()
     }
     timerEl.textContent = timeleft
 }
-function startgame (){
-    setInterval(tick,1000)
+function startgame() {
+    setInterval(tick, 1000)
     var startDiv = document.querySelector(".begin")
     startDiv.classList.add("hide")
     quizDiv.classList.remove("hide")
     askquestion()
 }
 
-function askquestion (){
+function askquestion() {
     document.getElementById("choices").innerHTML = ""
     var quizEl = document.getElementById("questionTitle")
     quizEl.textContent = questions[index].title
-    questions[index].choices.forEach(function(choice){
-    var btn = document.createElement("button")
-    btn.textContent = choice;
-    btn.setAttribute("value",choice)
-    btn.addEventListener("click",function(){
-        if(this.value === questions[index].answer){
-            console.log("correct")
-        }
-        else {
-            console.log("incorrect")
-            incorrect()
-        }
-        index++;
-        if (index === questions.length) {
-            console.log("endgame")
-            quizDiv.classList.add("hide")
-            timeleft = 0
-            endgame()
-        }
-        else {
-            askquestion()
-        }
-    })
-    document.getElementById("choices").appendChild(btn)
+    questions[index].choices.forEach(function (choice) {
+        var btn = document.createElement("button")
+        btn.textContent = choice;
+        btn.setAttribute("value", choice)
+        btn.addEventListener("click", function () {
+            if (this.value === questions[index].answer) {
+                console.log("correct")
+            }
+            else {
+                console.log("incorrect")
+                incorrect()
+            }
+            index++;
+            if (index === questions.length) {
+                console.log("endgame")
+                quizDiv.classList.add("hide")
+                timeleft = 0
+                endgame()
+            }
+            else {
+                askquestion()
+            }
+        })
+        document.getElementById("choices").appendChild(btn)
     })
 }
 
@@ -61,4 +61,20 @@ function endgame() {
     console.log("special")
 }
 
-startbtn.addEventListener("click",startgame)
+function saveHighscore() {
+    var initials = initialsEl.value.trim();
+    if (initials !== '') {
+        var highscores =
+            JSON.parse(window.localStorage.getItem('highscores')) || [];
+    }
+    var newScore = {
+        score: time,
+        initials: initials,
+    };
+    highscores.push(newScore);
+    window.localStorage.setItem('highscores', JSON.stringify(highscores));
+    window.location.href = 'highscores.html';
+}
+
+
+startbtn.addEventListener("click", startgame)
